423d06edda76ae0734fb4732e3f4d59ee5616d8c
Fakhrurrozi <fakhru2904@gmail.com>
2022-09-27 17:18:49 +0700

    Update and rename create_alist.py to a_list.py

--- a_list.py
+++ a_list.py
@@ -9,3 +9,13 @@ for i in range(5):
 for i in range(5):
   my_list.insert(0, i+1)
 # print [5, 4, 3, 2, 1]
+
+
+# count list
+my_list = [10, 1, 8, 3, 5]
+total = 0
+
+for i in range(len(my_list)):
+    total += my_list[i]
+
+# print(total) # 27

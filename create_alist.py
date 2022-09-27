9785cc0cd29bede75275042dc94a5d0f5f5b55f9
Fakhrurrozi <fakhru2904@gmail.com>
2022-09-27 17:17:17 +0700

    Create create_alist.py

---
+++ create_alist.py
@@ -0,0 +1,11 @@
+# first method
+my_list = []
+for i in range(5):
+  my_list.append(i+1)
+# print [1, 2, 3, 4, 5]
+
+  
+# second method
+for i in range(5):
+  my_list.insert(0, i+1)
+# print [5, 4, 3, 2, 1]
